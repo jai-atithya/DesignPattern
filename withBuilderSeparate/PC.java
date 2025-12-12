cc675e8ab024d81e9423e852b09c094098ca079d
Jai Atithya A <jaiatithya@gmail.com>
2025-12-12 19:47:51 +0530

    PC class added

---
+++ withBuilderSeparate/PC.java
@@ -0,0 +1,27 @@
+package withBuilderSeparate;
+
+public class PC {
+    private String cpu;
+    private String gpu;
+    private int ram;
+    private int storage;
+    private String os;
+    public PC(String cpu, String gpu, int ram, int storage, String os){
+        this.cpu=cpu;
+        this.gpu=gpu;
+        this.ram=ram;
+        this.storage=storage;
+        this.os=os;
+    }
+
+    @Override
+    public String toString() {
+        return "PC{" +
+                "cpu='" + cpu + '\'' +
+                ", gpu='" + gpu + '\'' +
+                ", ram=" + ram +
+                ", storage=" + storage +
+                ", os='" + os + '\'' +
+                '}';
+    }
+}
